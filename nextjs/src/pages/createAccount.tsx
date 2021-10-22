e19a9394f26882b862f01d876df339b26337fce3
= <=>
2021-10-22 17:00:34 -0400

    Removed excess components, create account allowed

--- nextjs/src/pages/createAccount.tsx
+++ nextjs/src/pages/createAccount.tsx
@@ -1,21 +1,24 @@
 import React from "react";
 import { CreateAccForm } from "../components/CreateAccForm";
 import Link from "next/link";
-import { GetServerSideProps } from "next";
-import { redirectIfUnauthenticated } from "../util/util";
 
-//https://nextjs.org/docs/basic-features/data-fetching#provided-req-middleware-in-getserversideprops
-export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-  const redirect = await redirectIfUnauthenticated(req);
-  if (redirect !== null) {
-    return {
-      redirect,
-    };
-  }
-  return {
-    props: { hello: "hello" }, // will be passed to the page component as props
-  };
-};
+// **UNCOMMENT to enable account creation only when a user is
+// logged in**
+// import { GetServerSideProps } from "next";
+// import { redirectIfUnauthenticated } from "../util/util";
+
+// //https://nextjs.org/docs/basic-features/data-fetching#provided-req-middleware-in-getserversideprops
+// export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+//   const redirect = await redirectIfUnauthenticated(req);
+//   if (redirect !== null) {
+//     return {
+//       redirect,
+//     };
+//   }
+//   return {
+//     props: {}, // will be passed to the page component as props
+//   };
+// };
 
 export default function CreateAccount() {
   return (
