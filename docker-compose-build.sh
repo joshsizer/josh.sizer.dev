838e3c4eecc290ef5ddbb2b162cebc7804bc1584
= <=>
2021-10-22 17:02:41 -0400

    Removed docker-compose-build.sh

--- docker-compose-build.sh
+++
@@ -1,8 +0,0 @@
-#  https://medium.com/@cybourgeoisie/docker-env-methods-for-passing-variables-through-docker-compose-801e6fdb4a75
- 
-if [ -f .env ]
-then
-    export $(cat .env | xargs)
-fi
-
-docker-compose build --build-arg APP_ENV=${APP_ENV}
